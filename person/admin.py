7a69b671539b54dfd33fbe31f72d23bc027ccd6e
gk-svg <gk32239@gmail.com>
2020-07-29 16:43:36 +0530

    admin tables

--- person/admin.py
+++ person/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Person,Country,State,City,Town
 # Register your models here.
+admin.site.register(Country)
+admin.site.register(State)
+admin.site.register(City)
+admin.site.register(Town)
+admin.site.register(Person)
