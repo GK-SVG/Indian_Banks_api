c35e856521f8575bf7080e5103f0d4f02129e0b7
gk-svg <gk32239@gmail.com>
2020-07-29 16:45:09 +0530

    models created

--- person/models.py
+++ person/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
-# Create your models here.
+    
+class Country(models.Model):
+    country=models.CharField(max_length=50)
+    description=models.TextField(max_length=300)
+    population=models.IntegerField()
+    gdp=models.FloatField()
+
+    def __str__(self):
+        return self.country
+    
+
+class State(models.Model):
+    country=models.ForeignKey(Country,on_delete=models.CASCADE)
+    state=models.CharField(max_length=50)
+    description=models.TextField(max_length=300)
+    population=models.IntegerField()
+    gdp=models.FloatField()
+
+    def __str__(self):
+        return self.state
+
+class City(models.Model):
+    country=models.ForeignKey(Country,on_delete=models.CASCADE)
+    state=models.ForeignKey(State,on_delete=models.CASCADE)
+    city=models.CharField(max_length=50)
+    description=models.TextField(max_length=300)
+    population=models.IntegerField()
+    gdp=models.FloatField()
+    pincode=models.CharField(max_length=10)
+    def __str__(self):
+        return self.city
+
+class Town(models.Model):
+    country=models.ForeignKey(Country,on_delete=models.CASCADE)
+    state=models.ForeignKey(State,on_delete=models.CASCADE)
+    town=models.CharField(max_length=50)
+    description=models.TextField(max_length=300)
+    population=models.IntegerField()
+    gdp=models.FloatField()
+    pincode=models.CharField(max_length=10)
+    def __str__(self):
+        return self.town
+
+class Person(models.Model):
+    county=models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.id
+    
